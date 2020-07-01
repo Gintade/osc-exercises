934f3244cc4db027e7159eed57d7e329679f7647
osc <smondragon@unal.edu.co>
2020-07-01 17:19:21 -0600

    fixed pid

---
+++ project2/part2/.tmp_versions/pid.mod
@@ -0,0 +1,2 @@
+/home/osc/Desktop/Ejercicios/osc-exercises/project2/part2/pid.ko
+/home/osc/Desktop/Ejercicios/osc-exercises/project2/part2/pid.o
--- project2/part2/pid.c
+++ project2/part2/pid.c
@@ -1,127 +1,38 @@
-/**
- * Kernel module that communicates with /proc file system.
- *
- * This provides the base logic for Project 2 - displaying task information
- */
-
-#include <linux/init.h>
-#include <linux/slab.h>
 #include <linux/sched.h>
-#include <linux/module.h>
+#include <linux/init.h>
 #include <linux/kernel.h>
-#include <linux/proc_fs.h>
-#include <linux/vmalloc.h>
-#include <asm/uaccess.h>
-#include <linux/uaccess.h>
-
-#define BUFFER_SIZE 128
-#define PROC_NAME "pid"
-
-/* the current pid */
-static long l_pid;
-
-/**
- * Function prototypes
- */
-static ssize_t proc_read(struct file *file, char *buf, size_t count, loff_t *pos);
-static ssize_t proc_write(struct file *file, const char __user *usr_buf, size_t count, loff_t *pos);
-
-static struct file_operations proc_ops = {
-        .owner = THIS_MODULE,
-        .read = proc_read,
-        .write = proc_write,
-};
+#include <linux/module.h>
 
 /* This function is called when the module is loaded. */
-static int proc_init(void)
-{
-        // creates the /proc/procfs entry
-        proc_create(PROC_NAME, 0666, NULL, &proc_ops);
+int entryPoint(void) {
+	struct task_struct *task; //Pointer to the task whose info will be printed
 
-        printk(KERN_INFO "/proc/%s created\n", PROC_NAME);
+	printk(KERN_INFO "Inserting ListTasksLinear\n");	
+	
+	printk(KERN_INFO "Started listing tasks\n");
 
+	for_each_process(task) { //Loop over the tasks using the macro for_each_process
+
+		/* on each iteration task points to the next task */
+		//task->comm is the task' name
+		//task->state is the task's state (-1 unrunnable, 0 runnable, >0 stopped)
+		//task->pid is the task's process ID
+		printk(KERN_INFO "Name: %-20s State: %ld\tProcess ID: %d\n", task->comm, task->state, task->pid);
+	}
+	
+	printk(KERN_INFO "Stopped listing tasks\n");
 	return 0;
 }
 
 /* This function is called when the module is removed. */
-static void proc_exit(void) 
-{
-        // removes the /proc/procfs entry
-        remove_proc_entry(PROC_NAME, NULL);
-
-        printk( KERN_INFO "/proc/%s removed\n", PROC_NAME);
-}
-
-/**
- * This function is called each time the /proc/pid is read.
- * 
- * This function is called repeatedly until it returns 0, so
- * there must be logic that ensures it ultimately returns 0
- * once it has collected the data that is to go into the 
- * corresponding /proc file.
- */
-static ssize_t proc_read(struct file *file, char __user *usr_buf, size_t count, loff_t *pos)
-{
-        int rv = 0;
-        char buffer[BUFFER_SIZE];
-        static int completed = 0;
-        struct task_struct *tsk = NULL;
-
-        if (completed) {
-                completed = 0;
-                return 0;
-        }
-
-        tsk = pid_task(find_vpid(l_pid), PIDTYPE_PID);
-
-        completed = 1;
-
-        /* return 0 if no this pid */
-        if( tsk == NULL)    return 0;
-        else rv = sprintf(buffer, "command = %s pid = %d state = %ld\n", tsk->comm, tsk->pid, tsk->state);
-
-        // copies the contents of kernel buffer to userspace usr_buf 
-        if (copy_to_user(usr_buf, buffer, rv)) {
-                rv = -1;
-        }
-
-        return rv;
-}
-
-/**
- * This function is called each time we write to the /proc file system.
- */
-static ssize_t proc_write(struct file *file, const char __user *usr_buf, size_t count, loff_t *pos)
-{
-        char *k_mem;
-
-        // allocate kernel memory
-        k_mem = kmalloc(count, GFP_KERNEL);
-
-        /* copies user space usr_buf to kernel buffer */
-        if (copy_from_user(k_mem, usr_buf, count)) {
-		printk( KERN_INFO "Error copying from user\n");
-                return -1;
-        }
-
-	/**
- 	 * kstrol() will not work because the strings are not guaranteed
-	 * to be null-terminated.
-	 * 
-	 * sscanf() must be used instead.
-	 */
-        sscanf(k_mem, "%ld", &l_pid);
-
-        kfree(k_mem);
-
-        return count;
+void exitPoint(void) {
+	printk(KERN_INFO "Removing ListTasksLinear\n");
 }
 
 /* Macros for registering module entry and exit points. */
-module_init( proc_init );
-module_exit( proc_exit );
+module_init(entryPoint);
+module_exit(exitPoint);
 
 MODULE_LICENSE("GPL");
-MODULE_DESCRIPTION("Module");
-MODULE_AUTHOR("SGG");
-
+MODULE_DESCRIPTION("List tasks linearly");
+MODULE_AUTHOR("Youssef Hassan, 900132871");
